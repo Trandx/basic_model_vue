385b956b57b7ae252c804ba27ce41d1027e7db03
Trandx <tsabimmo@gmail.com>
2022-11-25 10:21:18 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import tailwind css
+import './assets/css/tailwind.css'
+
+//import './assets/tailwind.css'
+
+createApp(App).mount('#app')
